ffb8d8d1559f28c0db438f0fbed89a401510358c
Amrut <amrutpanda7789@gmail.com>
2020-04-29 23:21:44 +0530

    employee monthly wage and working hour is added

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,76 +1,107 @@
-/**
+/*
 *Employee Wage Computation Program
-*calculated wages for a month
+*calculate wages till 20 days or 100 working hours reached
 *@author:Amrut
 */
 import java.util.Random;
 
-class EmpWageComputation{
-      public static void main(String []s){
-	int empType;
-	int dailyWage;
-	int monthlyWage=0;
-	int workingHrs;
+class EmpWageComputation3{
+      //main method
+      public static void main(String[] args){
+        int empType;
+		int dailyWage;
+		int workingHrs;
+	  //Totaldays variable counts days till 20 starting from day 1
+		int totalWorkingDays=1;
+		int loop=0;
+		int [] salaryAndHours = new int[2];
         System.out.println("Welcome to Employee Wage Computation Program");
-	EmpUtil eu = new EmpUtil();
-	Random ran = new Random();
-	empType = ran.nextInt(2);
-	//Storing the empHr returned in the variable workingHrs
-	workingHrs = eu.getEmpType(empType);
-	//Storing the daily wage in the variable DailyWage
-	dailyWage = eu.getDailyWage(workingHrs);
-	//getting monthly wage  
-        for (int i=1;i<=20;i++){
-            monthlyWage += eu.getMonthlyWage(workingHrs,i);
-	}
-        System.out.println("Employee monthy wage is: "+monthlyWage);
-      }
-}
-///Employee utility class
-class EmpUtil{
-int salary;
-int attendance;
-final int wagePerHr=20;
-int empHr;
+		EmpUtil eu = new EmpUtil();
+		Random ran = new Random();
+		empType = ran.nextInt(2);
 
-      //method to get employee is part time or full time
-      public int getEmpType(int type){
-       if(type==0){
-          System.out.println("FULL TIME EMPLOYEE");
-          empHr = 8;
-          return empHr;
-       }
-       else{
-          System.out.println("PART TIME EMPLOYEE");
-          empHr = 4;
-          return empHr;
-       }
-	  }
+		workingHrs = eu.getEmpType(empType);
+		//daily wage of employee
+		dailyWage = eu.getDailyWage(workingHrs);
+		System.out.println("The daily wage is: "+dailyWage);
 
-      //method to get employee monthly wage
-      public int getMonthlyWage(int Hour , int day)
-		  {
-			int status;
-			Random ran = new Random();
-			status = ran.nextInt(2);
-				if(status==0)
+         while (loop==0){
+
+            salaryAndHours = eu.getMonthlyWage(workingHrs,totalWorkingDays,empType);
+			//Loop till 20 days are over or 100 working hours completed
+            if(totalWorkingDays==20 || salaryAndHours[1]==100){
+				if(totalWorkingDays==20)
 					{
-						System.out.println("Day "+day+":Employee Present");					
-      					salary=wagePerHr*Hour;
-      					return salary;
+						System.out.println("20 days over!");
+						break;
 					}
 				else
 					{
-						System.out.println("Day "+day+": Employee Absent");
-						salary=0;																
-						return salary;
+						System.out.println("100 working hours completed!");
+						break;
 					}
-			}
-      //method to return employee daily wages
-      public int getDailyWage(int Hour)
-		  {
-			int wage;
-			wage=wagePerHr*Hour;
-			return wage;
-		}
+				}
+				totalWorkingDays+=1;
+         }
+		System.out.println("Employee worked for: "+salaryAndHours[1]+ "hours");
+		System.out.println("Employee monthly wage is: "+ salaryAndHours[0]);
+      }
 }
+//employee utility class
+class EmpUtil{
+int salary;
+int attendance;
+int workingHour;
+int empHr;
+final int wagePerHr=20;
+      
+	  //method to find employee type and return employee working hour
+      public int getEmpType(int type){
+        int empHr;
+        if(type==0){
+           System.out.println("FULL TIME EMPLOYEE");
+           empHr = 8;
+           return empHr;
+         }
+         else{
+            System.out.println("PART TIME EMPLOYEE");
+            empHr = 4;
+            return empHr;
+         }
+      }
+
+      //method to get employee monthly wage and total working hours
+      public int[] getMonthlyWage(int hour, int day, int type){
+       	int status;
+		Random ran = new Random();
+		status = ran.nextInt(2);
+		int[] salaryAndHours = new int[2];
+		if(status==0){
+			System.out.println("Day "+day+": Employee Present");
+			salary+=wagePerHr*hour;
+	    if(type==0)
+			workingHour+=8;
+	    if(type==1)
+			workingHour+=4;
+            salaryAndHours[0] = salary;
+			salaryAndHours[1] = workingHour;
+			return salaryAndHours;
+		}
+		else
+			{
+			System.out.println("Day "+day+": Employee Absent");
+			salary+=0;
+            workingHour+=0;
+			salaryAndHours[0] = salary;
+            salaryAndHours[1] = workingHour;
+            return salaryAndHours;
+		}
+      }
+      //method to get employee daily wages
+		public int getDailyWage(int hour){
+	    int wage;
+	    wage=wagePerHr*hour;
+            return wage;
+      }
+}
+
