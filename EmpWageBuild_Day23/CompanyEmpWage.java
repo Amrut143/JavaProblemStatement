88973408f49e2b2cd9841b72abc2ab6fc1dcf698
Amrut <amrutpanda7789@gmail.com>
2020-05-01 22:56:11 +0530

    company wage class is added

---
+++ EmpWageBuild_Day23/CompanyEmpWage.java
@@ -0,0 +1,24 @@
+/**
+*creating company employee wage to manage emp wage for multiple company
+*@author:Amrut
+*/
+
+public class CompanyEmpWage
+{
+	public final String company;
+	public final int empRatePerHr;
+	public final int numOfWorkingDays;
+	public final int maxHrPerMonth;
+	public int totalEmpWage;
+
+	//define constructor to initialize variables
+	public CompanyEmpWage(final String company,final int empRatePerHr,
+							final int numOfWorkingDays,final int maxHrPerMonth)
+	{
+		this.company=company;
+		this.empRatePerHr=empRatePerHr;
+		this.numOfWorkingDays=numOfWorkingDays;
+		this.maxHrPerMonth=maxHrPerMonth;
+		totalEmpWage=0;
+	}
+}
