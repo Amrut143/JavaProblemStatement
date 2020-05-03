8e693e8f479167169c1a1566a3ab8086d6e18ecf
Amrut143 <63304164+Amrut143@users.noreply.github.com>
2020-05-03 22:20:06 +0530

    Collection demo is added

--- CollectionDemo/CollectionDemo.java
+++ CollectionDemo/CollectionDemo.java
@@ -16,26 +16,26 @@ public class CollectionDemo
 {
 	public static void main(String[] args) 
 	{
-		//creating arraylist object
+		/*creating arraylist object*/
 		ArrayList al=new ArrayList();
 		System.out.println("ArrayList Examples");
-		//adding values to arraylist
+		/*adding values to arraylist*/
 		al.add("Amrut");
 		al.add("10");
 		al.add("Panda");
 		al.add(null);
 		System.out.println("initial arraylist"+al);//[Amrut, 10, Panda, null]
-		//get the value from second index
+		/*get the value from second index*/
 		System.out.println("2nd index value::"+al.get(2));//[Panda]
-		//set first index value
+		/*set first index value*/
 		al.set(1,20);
 		System.out.println("After setting the 1st index value::"+al);//[Amrut, 20, Panda, null]
-		//remove value from arraylist
+		/*remove value from arraylist*/
 		al.remove(3);
 		System.out.println("After removing 3rd indax value::"+al);
 		System.out.println();
 
-		//creating linkedlist object
+		/*creating linkedlist object*/
 		LinkedList<String> ll=new LinkedList<String>();
 		System.out.println("**********************************************");
 		System.out.println("LinkedList Examples");
@@ -43,35 +43,35 @@ public class CollectionDemo
 		ll.add("vijay");
 		ll.add("pradeep");
 		System.out.println("initial linkedlist::"+ll);
-		//set 1st index
+		/*set 1st index*/
 		ll.set(1,"software");
 		System.out.println("After setting 1st index::"+ll);
-		//remove element from last
+		/*remove element from last*/
 		ll.removeLast();
 		System.out.println("After removing last element::"+ll);
-		//adding element at first
+		/*adding element at first*/
 		ll.addFirst("developer");
 		System.out.println("After adding element at first::"+ll);
 		System.out.println();
 
-		//creating vector object
+		/*creating vector object*/
 		Vector<String> v=new Vector<String>(ll);
 		System.out.println("**************************************************");
 		System.out.println("initial capacity::"+v.capacity());
-		//adding element to vector
+		/*adding element to vector*/
 		v.addElement("Suresh");
 		System.out.println("vector after adding elements::"+v);
-		//get first element
+		/*get first element*/
 		System.out.println("first element is::"+v.firstElement());
-		//remove element from a specific index
+		/*remove element from a specific index*/
 		v.removeElementAt(1);
 		System.out.println("After removing::"+v);
 		System.out.println();
 
-		//creating stack object
+		/*creating stack object*/
 		Stack<String> s=new Stack<String>();
 		System.out.println("******************************************************");
-		//push elements into stack
+		/*push elements into stack*/
 		s.push("Amrut");
 		s.push("raja");
 		s.push("amit");
@@ -82,28 +82,27 @@ public class CollectionDemo
 		System.out.println("find stack is empty or not::"+s.empty());
 		System.out.println();
 
-		//creating hashset object
+		/*creating hashset object*/
 		HashSet<String> hs=new HashSet<String>(ll);
 		System.out.println("*****************************************************");
 		System.out.println(hs);
-		//adding elements to hash set
+		/*adding elements to hash set*/
 		hs.add("bikash");
-		//using cursor to iterate the values
+		/*using cursor to iterate the values*/
 		Iterator<String> itr=hs.iterator();
 		while(itr.hasNext())
 		{
 			System.out.println(itr.next());
 		}
-		//add duplicate element
+		/*add duplicate element*/
 		hs.add("bikash");
 		System.out.println("After adding duplicate element::"+hs);
-		//removing all the elements from hashset
+		/*removing all the elements from hashset*/
 		hs.removeAll(hs);
 		System.out.println("After removing all the elements from hashset::"+hs);
 		System.out.println();
 
-		//creating treeset object
-
+		/*creating treeset object*/
 		 TreeSet<String> set=new TreeSet<String>();
 		 System.out.println("*************************************************************");
          	set.add("Amrut");  
@@ -115,9 +114,9 @@ public class CollectionDemo
          {  
              System.out.println(i.next());  
          }
-		
-		//creating hashmap object
-		HashMap<Integer,String> hm=new HashMap<Integer,String>();    
+		/*creating hashmap object*/
+		Map<Integer,String> hm=new HashMap<Integer,String>();
+		System.out.println("***************************************************************");
       		hm.put(100,"Amit");    
       		hm.put(101,"Vijay");    
       		hm.put(102,"Rahul");   
@@ -125,5 +124,32 @@ public class CollectionDemo
 		hm.replace(102, "Gaurav");
 		System.out.println("After replacing 102 key value::"+hm);
 		
+		/*creating linkedhashmap object*/
+		 Map<Integer, String> lhm = new LinkedHashMap<Integer, String>();           
+      		 lhm.put(100,"Amrut");    
+     		 lhm.put(101,"Vijay");    
+     		 lhm.put(102,"Rahul");    
+      		 /*Fetching key*/  
+       		 System.out.println("Keys: "+lhm.keySet());  
+      		 /*Fetching value*/  
+      		 System.out.println("Values: "+lhm.values());  
+      		 /*Fetching key-value pair*/  
+       		 System.out.println("Key-Value pairs: "+lhm.entrySet());
+		 lhm.remove(102);  
+   		 System.out.println("After invoking remove() method: "+lhm);
+		
+		/*creating hashtable object*/
+		 Map<Integer,String> map=new Hashtable<Integer,String>();          
+    		 map.put(100,"Amit");    
+    		 map.put(102,"Ravi");   
+    		 map.put(101,"Vijay");    
+   	 	 map.put(103,"Rahul");    
+    		 System.out.println("Initial Map: "+map);  
+     		 /*Inserts, as the specified pair is unique*/  
+    		 map.putIfAbsent(104,"Gaurav");  
+    		 System.out.println("Updated Map: "+map);  
+    		 /*Returns the current value, as the specified pair already exist*/  
+    		 map.putIfAbsent(101,"Vijay");  
+    		 System.out.println("Updated Map: "+map);  
 	}
 }
